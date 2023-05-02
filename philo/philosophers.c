d030deff622874f6266b06882fded5deba98dd73
Abed <abed.hassani.idrissi@gmail.com>
2023-05-02 23:06:37 +0100

    mhassani

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: mhassani <mhassani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 20:34:09 by mhassani          #+#    #+#             */
-/*   Updated: 2023/04/12 00:09:22 by mhassani         ###   ########.fr       */
+/*   Updated: 2023/05/02 23:00:44 by mhassani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,13 @@ int main(int ac, char *av[])
 {
     if(ac < 4)
     {
-        write(2, "you should enter exaactly 4 arguments\n", 38);
+        write(2, "you should enter exactly 4 or 5 arguments\n", 42);
         exit(EXIT_FAILURE);
     }
     int n_philos = ft_atoi(av[1]);
-    int n_forks = ft_atoi(av[1]);
+    int n_forks = n_philos;
     int i = 0;
+    pthread_t philo[n_philos];
     if(n_philos < 1 || n_forks < 1)
     {
         write(2, "there is no philosopher at the table\n", 37);
@@ -35,17 +36,10 @@ int main(int ac, char *av[])
     }
     while(i < n_philos)
     {
-        pthread_t philo[i];
         pthread_create(&philo[i], NULL, philos, NULL);
         pthread_join(philo[i], NULL);
         i++;
     }
-    // only (n_philos / 2) can eat at the same time;
-    i = 0;
-    while (i < (n_philos / 2))
-    {
-        
-        i++;
-    }
+
     return 0;
 }
