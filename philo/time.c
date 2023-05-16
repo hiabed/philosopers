d785f6142519a5058ed919e4623abb87e90fd006
Abed <abed.hassani.idrissi@gmail.com>
2023-05-16 15:56:44 +0100

    mhassani

--- philo/time.c
+++ philo/time.c
@@ -6,7 +6,7 @@
 /*   By: mhassani <mhassani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 20:11:44 by mhassani          #+#    #+#             */
-/*   Updated: 2023/05/15 17:43:06 by mhassani         ###   ########.fr       */
+/*   Updated: 2023/05/16 14:45:26 by mhassani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,26 +33,26 @@ int	check_death(t_philo *ph)
 {
 	int	i;
 
-		i = 0;
-		pthread_mutex_lock(&ph->data->mutex2);
-		if (ph->data->n_times_eat != -1 && ph->data->n_philos
-			* ph->data->n_times_eat == ph->data->eating)
-			return (0);
-		pthread_mutex_unlock(&ph->data->mutex2);
-		while (i < ph->data->n_philos)
+	i = 0;
+	pthread_mutex_lock(&ph->data->mutex2);
+	if (ph->data->n_times_eat != -1 && ph->data->n_philos
+		* ph->data->n_times_eat == ph->data->eating)
+		return (0);
+	pthread_mutex_unlock(&ph->data->mutex2);
+	while (i < ph->data->n_philos)
+	{
+		pthread_mutex_lock(&ph->data->mutex);
+		if (timee() - ph[i].last_meal >= ph->data->t_die)
 		{
-			pthread_mutex_lock(&ph->data->mutex);
-			if (timee() - ph[i].last_meal >= ph->data->t_die)
-			{
-				pthread_mutex_lock(&ph->data->print);
-				printf("%lu %d  died\n", timee() - ph->data->first_time,
-					ph[i].id);
-				return (0);
-			}
-			pthread_mutex_unlock(&ph->data->mutex);
-			i++;
+			pthread_mutex_lock(&ph->data->print);
+			printf("%lu %d  died\n", timee() - ph->data->first_time,
+				ph[i].id);
+			return (0);
 		}
-		return (1);
+		pthread_mutex_unlock(&ph->data->mutex);
+		i++;
+	}
+	return (1);
 }
 
 void	ft_end(t_data *data)
